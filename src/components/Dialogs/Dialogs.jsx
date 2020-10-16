d54b9b081c1ece82a356e280db42bf9d2d630dc0
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-16 21:40:10 +0300

    добавил компонент имена чатов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,23 +2,39 @@ import React from 'react';
 import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
+
+const Chat = (props) => {
+
+    let path = "/dialogs/" + props.id;
+
+    return (
+        <div><NavLink activeClassName={s.active} to={path}>{props.chatName}</NavLink></div>
+    );
+}
+
+const Message = (props) => {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
 const Dialogs = (props) => {
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogName}>
-                <div className={s.dialog}>
-                                          <NavLink activeClassName={s.active} to="/dialogs/1">Nikita</NavLink></div>
-                <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/2">Dima</NavLink></div>
-                <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/3">Vasya</NavLink></div>
-                <div className={s.dialog}><NavLink activeClassName={s.active} to="/dialogs/4">Gena</NavLink></div>
+        <div className={s.dialogsWrapper}>
+            <div className={s.dialogsWindow}>
+                <Chat id="1" chatName="Nikita"/>
+                <Chat id="2" chatName="Dima"/>
+                <Chat id="3" chatName="Sasha"/>
+                <Chat id="4" chatName="Lesha"/>
             </div>
-            <div className={s.dialogMessage}>
-                <div className={s.message}>Hello!</div>
-                <div className={s.message}>How are you?</div>
-                <div className={s.message}>I KILL YOU</div>
+
+            <div className={s.chatWindow}>
+                <Message message="Hi!" />
+                <Message message="How are you?" />
+                <Message message="I'll found you!" />
             </div>
         </div>
-    )
+    );
 }
 
 export default Dialogs;
