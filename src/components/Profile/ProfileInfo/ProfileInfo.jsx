135935db7c4c606926e3e9f6f0ac4700ff152d47
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-18 16:40:28 +0300

    решил проблему

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import s from "./ProfileInfo.module.css";
 
 const ProfileInfo = () => {
     return (
         <div>
             <img
                 src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
-            <div>description</div>
+            <div className={s.descriptionBlock}>description</div>
         </div>
     );
 }
