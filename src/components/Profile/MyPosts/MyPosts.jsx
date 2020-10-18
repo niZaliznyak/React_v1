1f82facf6c31eb81dcdcabef413a80fe156b0767
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-18 16:51:42 +0300

    16^51

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,25 +3,24 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
-  return <div className={s.content}>
+    return <div className={s.content}>
+        <div>
+            My posts
+        </div>
 
-    <div>
+        <textarea placeholder="Type something"></textarea>
+        <div>
+            <button>Add post</button>
+            <button>Remove</button>
+        </div>
 
-      <textarea placeholder="Type something"></textarea>
-      <button>Add post</button>
-      <button>Remove</button>
+        <div className={s.posts}>
+            <Post message="Hello my friends" like="25"/>
+            <Post message="I learn react" like="7"/>
+            <Post message="How many years gone..." like="30"/>
+        </div>
 
-      <div>
-        New post
-      </div>
-      <div className={s.posts}>
-        <Post message="Hello my friends" like="25"/>
-        <Post message="I learn react" like="7"/>
-        <Post message="How many years gone..." like="30"/>
-      </div>
     </div>
-
-  </div>
 
 }
 
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -6,7 +6,7 @@ const ProfileInfo = () => {
         <div>
             <img
                 src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
-            <div className={s.descriptionBlock}>description</div>
+            <div className={s.descriptionBlock}>description about myself</div>
         </div>
     );
 }
