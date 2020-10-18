8968136cffc9958ff859fe624d9ef3b5db770cf4
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-18 16:25:47 +0300

    create component ProfileInfo

---
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ProfileInfo = () => {
+    return (
+        <div>
+            <img
+                src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+            <div>description</div>
+        </div>
+    );
+}
+
+export default ProfileInfo;
