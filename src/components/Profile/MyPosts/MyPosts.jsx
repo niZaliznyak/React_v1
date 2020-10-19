e96fabbf35ecdda462fa2188b0c72b5a5b2bcfa5
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-19 16:36:52 +0300

    add postBaseData varieble

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
+let postsDataBase = [
+    {id : 0, text : 'Hello my friends!', like : 25},
+    {id : 1, text : 'I learn react', like : 13},
+    {id : 2, text : 'Far far away...', like : 20},
+];
+
+
 const MyPosts = () => {
     return <div className={s.content}>
         <div>
@@ -15,9 +22,9 @@ const MyPosts = () => {
         </div>
 
         <div className={s.posts}>
-            <Post message="Hello my friends" like="25"/>
-            <Post message="I learn react" like="7"/>
-            <Post message="How many years gone..." like="30"/>
+            <Post message={postsDataBase[0].text} like={postsDataBase[0].like}/>
+            <Post message={postsDataBase[1].text} like={postsDataBase[1].like}/>
+            <Post message={postsDataBase[2].text} like={postsDataBase[2].like}/>
         </div>
 
     </div>
