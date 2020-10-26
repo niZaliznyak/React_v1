7d55f435bddfc644b6e9068c021a9e8850b55049
niZaliznyak <niZaliznyak@users.noreply.github.com>
2020-10-26 19:13:21 +0200

    упростил скрипт указывающий отправителя сообщения

--- src/components/Dialogs/ChatItem/ChatItem.jsx
+++ src/components/Dialogs/ChatItem/ChatItem.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import s from './ChatItem.module.css';
 
 const ChatItem = (props) => {
-    if (props.sender == true) {
-        return (
-            <div className={s.sender}>{props.message}</div>
-        );
-    } else {
-        return (
-            <div className={s.answer}>{props.message}</div>
-        );
-    }
+    let status = props.sender == true ? s.sender : s.answer;
+    return (
+        <div className={status}>{props.message}</div>
+    );
+
 }
 
 export default ChatItem;
