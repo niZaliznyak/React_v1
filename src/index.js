71fbdac779f8f2d80fa9dbd2ce03738ea59488d9
niZaliznyak <novieasdf@gmail.com>
2020-11-13 22:30:21 +0200

    create store-file width method's and use bind()

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import {addToPostsDataBase, updatePostText} from "./redux/state";
 
 let reRenderEntireTree = (state) => {
     ReactDOM.render(<BrowserRouter>
-        <App state={state} addToPostsDataBase={addToPostsDataBase} updatePostText={updatePostText}/>
+        <App state={state}
+             addToPostsDataBase={store.addToPostsDataBase.bind(store)}
+             updatePostText={store.updatePostText.bind(store)}/>
     </BrowserRouter>, document.getElementById('root'));
 };
 
-reRenderEntireTree(state);
+reRenderEntireTree(store.getState());
 
-subscribe(reRenderEntireTree);
+store.subscribe(reRenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,6 +1,66 @@
 import React from 'react';
 
-let reRenderEntireTree = () => console.log("");
+let store = {
+    _state : {
+        dialogsPage: {
+            dialogsNamesData: [
+                {id: 1, name: 'Nikita', avatar: 'https://avas.at.ua/_ph/45/1/469869713.jpg?1603712491'},
+                {id: 2, name: 'Egor', avatar: 'https://avas.at.ua/_ph/45/1/563058985.jpg?1603712491'},
+                {id: 3, name: 'Dima', avatar: 'https://avas.at.ua/_ph/45/1/940876170.jpg?1603712670'},
+            ],
+
+            messageDataBase: [
+                {id: 1, sender: true, text: "Hello!"},
+                {id: 2, sender: true, text: "How are you?"},
+                {id: 3, sender: false, text: "I'll find you and hug!"},
+                {id: 4, sender: true, text: "Oh, no!"},
+                {id: 5, sender: false, text: "Time is running..."},
+            ]
+        },
+
+        profilePage: {
+            postsDataBase: [
+                {id: 0, text: 'Hello my friends!', like: 25},
+                {id: 1, text: 'I learn react', like: 13},
+                {id: 2, text: 'Far far away...', like: 20},
+            ],
+            postTypingText: ""
+        }
+    },
+
+    getState(){
+        return this._state;
+    },
+
+    _callSubscriber() {
+        console.log("")
+    },
+
+    addToPostsDataBase(){
+        let newPost = {
+            id: 3,
+            text: this._state.profilePage.postTypingText,
+            like: 0
+        };
+        this._state.profilePage.postsDataBase.push(newPost);
+        this._state.profilePage.postTypingText = '';//после добавления поста, очищает textarea
+        this._callSubscriber(this._state);
+    },
+
+    updatePostText(typetext){
+        this._state.profilePage.postTypingText = typetext;
+        debugger;
+        this._callSubscriber(this._state);
+    },
+
+    subscribe(observer){
+        this._callSubscriber = observer;
+    }
+}
+
+window.store = store;
+export default store;
+/*
 
 let state = {
     dialogsPage: {
@@ -29,6 +89,8 @@ let state = {
     }
 }
 
+let reRenderEntireTree = () => console.log("");
+
 export const addToPostsDataBase = () => {
     let newPost = {
         id: 3,
@@ -48,5 +110,6 @@ export const updatePostText = (typetext) => {
 export const subscribe = (observer) => {
     reRenderEntireTree = observer;
 }
+*/
 
-export default state;
+
