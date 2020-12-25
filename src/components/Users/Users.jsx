673ae9c7c33eea284bd323fedcf8e2735cfe0487
niZaliznyak <novieasdf@gmail.com>
2020-12-25 17:14:00 +0200

    menor changes

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,12 +3,15 @@ import styles from "./Users.module.css";
 import * as axios from "axios";
 import AvatarIMG from '../../assets/img/simple-avatar.png';
 
+//в классовую компоненту не приходят props. Props приходят в уже отрисованый jsx. Поэтому тут обращаемся к пропсам через this.
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items);
-        });
+
+    componentDidMount() {
+        if (this.props.usersData.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                this.props.setUsers(response.data.items);
+            });
+        }
     }
 
     render() {
