b8d901660c40308e75557e0f5375d1e01a1e3ebc
niZaliznyak <novieasdf@gmail.com>
2021-01-07 20:37:25 +0200

    ls:56 done!

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,58 +1,42 @@
 import React from 'react';
 import styles from "./Users.module.css";
-import * as axios from "axios";
 import AvatarIMG from '../../assets/img/simple-avatar.png';
 
-//в классовую компоненту не приходят props. Props приходят в уже отрисованый jsx. Поэтому тут обращаемся к пропсам через this.
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        if (this.props.usersData.length === 0) {
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-                this.props.setUsers(response.data.items);
-            });
-        }
+    let pages = [];
+    for (let i = 1; i <= Math.ceil(props.totalUsersCount / props.pageSize); i++) {
+        pages.push(i);
     }
 
-    onPageChange = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
-    }
-
-    render() {
-
-        let pages = [];
-        for (let i = 1; i <= Math.ceil(this.props.totalUsersCount / this.props.pageSize); i++ ){
-            pages.push(i);
-        }
-
-        return <div>
-            <div>
-                {pages.map(p => {
-                    return <span className={this.props.currentPage === p ? styles.selectedPage : ""}
-                                 onClick={(e) => {this.onPageChange(p)} }>{p}</span>
-                })}
-            </div>
-            {this.props.usersData.map(elem => (
-                    <div className={styles.userElement} key={elem.id}>
-                        <div><img alt="userPhoto" className={styles.userPhoto} src={AvatarIMG}></img></div>
-                        <div>Nickname: {elem.name}; id: {elem.id}; status: {elem.status}</div>
-                        <div>{elem.follow ?
-                            <button onClick={() => {
-                                this.props.unfollow(elem.id)
-                            }}>unfollow</button>
-                            : <button onClick={() => {
-                                this.props.follow(elem.id)
-                            }}>follow</button>
-                        }</div>
-                    </div>
-                )
-            )}
+    return <div>
+        <div>
+            {pages.map(p => {
+                return <span className={props.currentPage === p ? styles.selectedPage : ""}
+                             onClick={(e) => {
+                                 props.onPageChange(p)
+                             }}>{p}</span>
+            })}
         </div>
-    }
-}
 
+        {props.usersData.map(elem => (
+                <div className={styles.userElement} key={elem.id}>
+                    <div><img alt="userPhoto" className={styles.userPhoto} src={AvatarIMG}></img></div>
+                    <div>Nickname: {elem.name}; id: {elem.id}; status: {elem.status}</div>
+                    <div>{elem.follow ?
+                        <button onClick={() => {
+                            props.unfollow(elem.id)
+                        }}>unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(elem.id)
+                        }}>follow</button>
+                    }</div>
+                </div>
+            )
+        )}
+
+    </div>
+
+}
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setUsersAC, unfollowAC} from "../../redux/usersReducers";
+import * as axios from "axios";
 import Users from "./Users";
 
 let mapStateToProps = (state) => {
@@ -29,6 +30,33 @@ let mapDispatchToProps = (dispatch) => {
     };
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+//в классовую компоненту не приходят props. Props приходят в уже отрисованый jsx. Поэтому тут обращаемся к пропсам через this.
+class UsersAPIComponent extends React.Component {
+
+    componentDidMount() {
+        if (this.props.usersData.length === 0) {
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+            });
+        }
+    }
+
+    onPageChange = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize} currentPage={this.props.currentPage}
+                      usersData={this.props.usersData} follow={this.props.follow} unfollow={this.props.unfollow}
+                      onPageChange={this.onPageChange}
+        />
+    }
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPIComponent);
 
 export default UsersContainer;
