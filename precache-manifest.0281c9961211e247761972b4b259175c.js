b24015a898459b57f11585c79acd08a10989f574
niZaliznyak <novieasdf@gmail.com>
2021-06-07 20:27:49 +0300

    Updates

---
+++ precache-manifest.0281c9961211e247761972b4b259175c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa0cb99f27606a021d37",
+    "url": "/React_v1/static/css/main.edb50cfe.chunk.css"
+  },
+  {
+    "revision": "fa0cb99f27606a021d37",
+    "url": "/React_v1/static/js/main.fa0cb99f.chunk.js"
+  },
+  {
+    "revision": "2c50b4f5b574008d8660",
+    "url": "/React_v1/static/css/1.b3befeb3.chunk.css"
+  },
+  {
+    "revision": "2c50b4f5b574008d8660",
+    "url": "/React_v1/static/js/1.2c50b4f5.chunk.js"
+  },
+  {
+    "revision": "1df956fd86ec9b0b6579",
+    "url": "/React_v1/static/js/runtime~main.1df956fd.js"
+  },
+  {
+    "revision": "da8216940b0de45a02eb399e25dc4e27",
+    "url": "/React_v1/static/media/simple-avatar.da821694.png"
+  },
+  {
+    "revision": "7f08f2ac57022523fb0dc5cd2401bbd3",
+    "url": "/React_v1/index.html"
+  }
+];
